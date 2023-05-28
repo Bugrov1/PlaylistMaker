26dd00065369be8250445198aa6414c85a036f71
Oleg Bugrov <px316@yandex.ru>
2023-05-29 01:22:38 +0300

    2

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,48 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val backButton = findViewById<ImageButton>(R.id.back)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        backButton.setOnClickListener{
+            finish()
+        }
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            inputEditText.onEditorAction(EditorInfo.IME_ACTION_DONE)
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+
+            }
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,8 +2,10 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
@@ -20,6 +22,36 @@ class SettingsActivity : AppCompatActivity() {
             finish()
             //startActivity(displayIntent)
         }
+        val shareButton=findViewById<FrameLayout>(R.id.share_button)
+        shareButton.setOnClickListener {
+            val message = getString(R.string.share_button_message)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type="text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT,message)
+            startActivity(shareIntent)
+
+        }
+        val supportButton=findViewById<FrameLayout>(R.id.support_button)
+        supportButton.setOnClickListener {
+            val mailAdress = getString(R.string.support_mail_to)
+            val mailTheme = getString(R.string.support_theme_message)
+            val mailText = getString(R.string.support_text_message)
+            val supportIntent = Intent(Intent.ACTION_SEND)
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(mailAdress))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, mailTheme)
+            supportIntent.putExtra(Intent.EXTRA_TEXT, mailText)
+            startActivity(supportIntent)
+        }
+        val licenseButton=findViewById<FrameLayout>(R.id.users_license)
+        licenseButton.setOnClickListener {
+            val link = getString(R.string.users_licence_link)
+            val uri = Uri.parse(link)
+            val usersIntent = Intent(Intent.ACTION_VIEW,uri)
+            startActivity(usersIntent)
+            //написать интент
+        }
+
     }
 
 }
